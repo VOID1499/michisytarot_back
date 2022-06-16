88a7d276db943347894d4b342e273ef5703ea439
erick <erick.olviares.gonzalez@gmail.com>
2022-06-16 17:30:50 -0400

    1

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    
+
+        public function __construct()
+        {
+        
+        }
+
+        public function registrarUsuario(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'code'=> 1,
+                'message'=>  $validator->errors(),
+            ],400);
+        }
+
+        try{
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+               ]);
+        
+               $token = $user->createToken('auth_token')->plainTextToken;
+        
+               return response()->json([
+                'code'=> 0,
+                'message'=>'Usuario creado',
+                'user'=>$user,
+                'access_token'=>$token,
+                'user'=>$user,
+                'token_type' =>'Bearer'
+               ],200);
+
+        }catch(Exception $e){
+
+        }
+     
+
+    }
+
+
+    public function login(Request $request){
+
+        if(!Auth::attempt($request->only('email','password'))){
+
+            return response()->json([
+                'code' => 0,
+                'message' => 'Sin autorización',
+            ],401);
+
+        }
+        
+        $user = User::where('email','=',$request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'code'=> 0,
+            'message'=>'Usuario logeado',
+            'access_token'=>$token,
+            'user'=>$user,
+            'token_type' =>'Bearer'
+        ]);
+
+    }
+
+
+    public function logout(){
+
+        //no problemo
+       auth()->user()->tokens()->delete();
+
+            return response()->json([
+                'code'=> 0,
+                'message'=>'Usuario deslogeado, tokens eliminados',
+            ]);
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user',function(Request $request){
+    return $request->user();
+});
+
+Route::post('login',[AuthController::class,'login']); 
+
+Route::group(['middleware'=>['auth:sanctum']],function(){
+    Route::post('registrarUsuario',[AuthController::class,'registrarUsuario']);
+    Route::get('logout',[AuthController::class,'logout']);
+});
+
+
+
+    
